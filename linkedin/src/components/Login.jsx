919cd00807796fafaf3017f80b3084c10f2cca40
3sandre <sandrinetohe5@gmail.com>
2022-08-22 14:42:40 +0000

    ajout de fonction à déclanché au click de sign whith google

--- linkedin/src/components/Login.jsx
+++ linkedin/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { connect } from "react-redux";
-import { signInAPI } from "../actions";
+import { signInAPI } from '../actions';
 
 
 const Login = (props) => {
@@ -21,7 +21,7 @@ const Login = (props) => {
                     <img src="/images/login-hero.svg" alt="" />
                 </Hero>
                 <Form>
-                    <Google onChange={() => props.signIn()}>
+                    <Google onClick={() => props.signIn()}>
                         <img src="/images/google.svg" alt="" />
                         Sign in with Google
                     </Google>
--- linkedin/src/firebase.js
+++ linkedin/src/firebase.js
@@ -1,19 +1,26 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCwYqb-CswxEqz5DDGjQ-t77kPlCHHeGM",
-    authDomain: "linkedin-5199d.firebaseapp.com",
-    projectId: "linkedin-5199d",
-    storageBucket: "linkedin-5199d.appspot.com",
-    messagingSenderId: "964357315535",
-    appId: "1:964357315535:web:590a8646e56b6ed72690ba"
-  };
+  apiKey: "AIzaSyDCwYqb-CswxEqz5DDGjQ-t77kPlCHHeGM",
+  authDomain: "linkedin-5199d.firebaseapp.com",
+  projectId: "linkedin-5199d",
+  storageBucket: "linkedin-5199d.appspot.com",
+  messagingSenderId: "964357315535",
+  appId: "1:964357315535:web:590a8646e56b6ed72690ba",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
-  const storage = firebase.storage();
+// Initialize Firebase
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  export {auth, provider, storage};
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+//const storage = firebase.storage();
+
+export { auth, provider, db };
   export default db;
