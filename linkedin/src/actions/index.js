b32de3b49bdb8e17fdd1e89ade5031156e61c15b
3sandre <sandrinetohe5@gmail.com>
2022-08-24 09:14:39 +0000

    photo de login s'il y a lieu

--- linkedin/src/actions/index.js
+++ linkedin/src/actions/index.js
@@ -11,8 +11,19 @@ export function signInAPI() {
         auth
         .signInWithPopup(provider)
         .then((payload) => {
-            dispatch(setUser(payload.user));
+            console.log(payload.user);
+            // dispatch(setUser(payload.user));
         })
         .catch((error) => alert(error.message));
+    };
+}
+
+export function getUserAuth() {
+    return (dispatch) =>{
+        auth.onAuthStateChanged(async (user) =>{
+            if(user) {
+                dispatch(setUser(user));
+            }
+        });
     };
 }
--- linkedin/src/components/Header.jsx
+++ linkedin/src/components/Header.jsx
@@ -1,6 +1,5 @@
+import { connect } from "react-redux";
 import styled from "styled-components";
-
-
 
 const Header = (props) => {
     return (
@@ -58,9 +57,11 @@ const Header = (props) => {
 
                         <User>
                             <a>
-                                <img src="/images/user.svg" alt="" />
-                                <span>Me</span>
-                                <img src="/images/down-icon.svg" alt="" />
+                                {props.user && props.user.photoURL ? (<img src={props.user.photoURL} alt=""/>) : (<img src="/images/user.svg" alt="" />)}
+                                <span>Me
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </span>
+                                
                             </a>
 
                             <SignOut>
@@ -252,4 +253,12 @@ const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-export default Header;
+const mapStateToProps = (state) => {
+    return{
+        user: state.userState.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect (mapStateToProps, mapDispatchToProps)(Header);
